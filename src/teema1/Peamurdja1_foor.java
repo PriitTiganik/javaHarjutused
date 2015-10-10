85883898021779c31f0ac6180b098539539068be
Priit Tiganik <ptiganik@klass3-10-l.intra.itcollege.ee>
2015-10-10 15:19:50 +0300

    praktikum1

--- src/teema1/Peamurdja1_foor.java
+++ src/teema1/Peamurdja1_foor.java
@@ -1,5 +1,7 @@
 package teema1;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
 import lib.Foor;
 
 /**
@@ -17,5 +19,37 @@ import lib.Foor;
  * NB! Kui kirjutad new Foor(); siis IDE pakub sulle Foori importimise võimalust.
  * Kliki punase teksti peale ja vajuta alt+enter.
  */
-public class Peamurdja1_foor {
+public class Peamurdja1_foor extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Foor foor = new Foor(Foor.VASAK, primaryStage);
+        // viis tsüklit
+        int i=0;
+
+        while (i <5)   {
+        foor.syytaPunane();
+            foor.paus(2);
+        foor.syytaKollane();
+            foor.paus(0.5);
+        foor.kustutaPunane();
+        foor.kustutaKollane();
+
+        foor.vahetaRohelist();
+            foor.paus(2);
+        foor.vahetaRohelist();
+            foor.paus(0.5);
+        foor.vahetaRohelist();
+            foor.paus(0.5);
+        foor.vahetaRohelist();
+            foor.paus(0.5);
+        foor.kustutaRoheline();
+        foor.syytaKollane();
+            foor.paus(0.5);
+        foor.syytaPunane();
+        foor.kustutaKollane();
+        i = i+1;
+        }
+    }
+
 }
--- src/teema1/Peamurdja2_ristmik.java
+++ src/teema1/Peamurdja2_ristmik.java
@@ -1,8 +1,48 @@
 package teema1;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+import lib.Foor;
+
 /**
  * Täpselt nagu oli fooriga, on nüüd ristmikuga. Kuidas esiteks
  * ristmik üles ehitada loe Foori README.md-st: https://github.com/KristerV/javaHarjutused
  */
-public class Peamurdja2_ristmik {
+
+public class Peamurdja2_ristmik extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Foor foorvasak = new Foor(Foor.VASAK, primaryStage);
+        Foor foorparem = new Foor(Foor.PAREM, primaryStage);
+        Foor foor = foorvasak;
+        foor = foorparem;
+        // viis tsüklit
+        int i=0;
+
+        while (i <2)   {
+            foor.syytaPunane();
+            foor.paus(2);
+            foor.syytaKollane();
+            foor.paus(0.5);
+            foor.kustutaPunane();
+            foor.kustutaKollane();
+
+            foor.vahetaRohelist();
+            foor.paus(2);
+            foor.vahetaRohelist();
+            foor.paus(0.5);
+            foor.vahetaRohelist();
+            foor.paus(0.5);
+            foor.vahetaRohelist();
+            foor.paus(0.5);
+            foor.kustutaRoheline();
+            foor.syytaKollane();
+            foor.paus(0.5);
+            foor.syytaPunane();
+            foor.kustutaKollane();
+            i = i+1;
+        }
+    }
+
 }
